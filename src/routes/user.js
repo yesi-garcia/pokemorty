76fead39bcd5746d522c2dcc08117df42a29c305
Yesenia <yesenialinares24@gmail.com>
2022-09-03 16:34:27 -0400

    se creo archivo helpers instaciaAxios.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const { handleHttpError } = require('../helpers/handleError');
 const { validarLogin, validarRegistro } = require('../validators/user');
 const { registrarUser } = require('../controllers/user');
-
+const intanciaAxios = require('../helpers/intanciaAxios');
 
 router.post("/registro", validarRegistro, registrarUser);
 
